de25fd235930ec3145f37d770ba4eb159d64d06a
zmachine82 <zmachine82@gmail.com>
2018-08-18 09:50:23 -0500

    add a twitter link

--- spec/features/list_events_spec.rb
+++ spec/features/list_events_spec.rb
@@ -9,7 +9,7 @@ describe "Viewing the list of events" do
 
     visit events_url
 
-    expect(page).to have_text("3 Events")
+    
     expect(page).to have_text(event1.name)
     expect(page).to have_text(event2.name)
     expect(page).to have_text(event3.name)
