f28abe414002953ed2ddb965e42457ce8dce945f
zmachine82 <zmachine82@gmail.com>
2018-09-25 10:38:17 -0500

    basic user auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_18_184743) do
+ActiveRecord::Schema.define(version: 2018_09_25_153608) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_08_18_184743) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
