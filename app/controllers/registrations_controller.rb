c39186cad7383b69e98f639307938ede0b0eba7d
zmachine82 <zmachine82@gmail.com>
2018-09-26 10:25:15 -0500

    changed resgistrations to use user data instead of name and email

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < ApplicationController
   before_action :set_event
+  before_action :require_signin
 
   def index
     @registrations = @event.registrations
@@ -11,6 +12,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = @event.registrations.new(registration_params)
+    @registration.user = current_user
     if @registration.save
       redirect_to event_registrations_path(@event),
                 notice: "Thanks, you're registered!"
@@ -22,7 +24,7 @@ class RegistrationsController < ApplicationController
 private
 
 def registration_params
-  params.require(:registration).permit(:name, :email, :how_heard)
+  params.require(:registration).permit(:how_heard)
 end
 
 def set_event
