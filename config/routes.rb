6f5d389b360764bb82a1b6331baca1daecfc5fa6
zmachine82 <zmachine82@gmail.com>
2018-10-01 10:04:13 -0500

    customizing routes for scopes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
+
+  get "events/filter/:scope" => "events#index", as: :filtered_events
+
   resources :events do
     resources :registrations
     resources :likes
